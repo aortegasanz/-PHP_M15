2618a848b2681acdf725679d4e29051e2f5c1fe5
Alberto Ortega <ortegasanz@gmail.com>
2021-06-28 00:02:55 +0200

    Level 1 complete. Logic review and validate fields

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -15,11 +15,34 @@ class PlayerController extends Controller
     //  Params ($request)
     //  name (string)
     //  type (1: Registered Player | 0: Anonymous)
-    public function store(Request $request) {
-        $player = Player::create([
-            'name' => $request->name,
-            'type' => $request->type,
-        ]);
+    public function store(Request $request) {        
+        if ($request->type == 1) {
+            // Type = 1 (Named Player)
+            $player = Player::where('name', $request->name)->first();
+            if (!$player) {
+                $player = Player::create([
+                  'name' => $request->name,
+                  'type' => $request->type,
+                ]);
+            } else {
+                return response()->json([
+                  'success' => false,
+                  'error' => 'Duplicated Player Name ('.$request->name.')',
+                ], 500);
+            }
+        } elseif ($request->type == 0) {
+            // Type = 0 (Anonymous Player)
+            $player = Player::create([
+                'name' => 'Anonymous',
+                'type' => $request->type,
+            ]);
+        } else {
+          return response()->json([
+              'success' => false,
+              'error' => 'Undefined Player Type ('.$request->type.')',
+          ], 500);
+        }
+        
         return response()->json(compact('player'));
     }
 
@@ -32,99 +55,143 @@ class PlayerController extends Controller
 
     /* Retorna el llistat de tots els jugadors del sistema amb el seu percentatge mig d’èxits */
     public function list() {
-        $sql = "SELECT player_id, P.name, count(*) as Total,
-                 (select count(*)
-                    from games as tempG
-                   where tempG.player_id = G.player_id
-                     and tempG.status = 0
-                   group by status) as LoserShoots,
-                 (select count(*)
-                    from games as tempG
-                   where tempG.player_id = G.player_id
-                     and tempG.status = 1
-                   group by status) as WinnerShoots,
-                 ((select count(*)
-                    from games as tempG
-                   where tempG.player_id = G.player_id
-                     and tempG.status = 1
-                   group by status) * 100) / count(*) as Promedio
-            FROM games as G join players as P on (G.player_id = P.id)
-           GROUP BY player_id, P.name";
+        $sql = "SELECT P.id, P.name,
+                      (select count(*)
+                        from games as tempG1
+                        where tempG1.player_id = P.id
+                        group by tempG1.player_id) as Total,
+                      (select count(*)
+                        from games as tempG2
+                        where tempG2.player_id = P.id
+                          and tempG2.status = 0
+                        group by status) as LoserShoots,
+                      (select count(*)
+                        from games as tempG3
+                        where tempG3.player_id = P.id
+                          and tempG3.status = 1
+                        group by status) as WinnerShoots,
+                      (
+                      (select count(*)
+                        from games as tempG4
+                        where tempG4.player_id = P.id
+                          and tempG4.status = 1
+                        group by status) * 100)
+                      /
+                      (select count(*)
+                        from games as tempG5
+                        where tempG5.player_id = P.id
+                        group by tempG5.player_id)
+                      as Promedio
+                 FROM games as G right join players as P on (G.player_id = P.id)
+                GROUP BY P.id, P.name";
         $ranking = DB::select($sql);
         return response()->json(['ranking' => $ranking], 200);
     }
 
     /* Retorna el ranking mig de tots els jugadors del sistema. És a dir, el percentatge mig d’èxits. */
     public function ranking() {
-        $sql = "SELECT player_id, P.name, count(*) as Total,
-                 (select count(*)
-                    from games as tempG
-                   where tempG.player_id = G.player_id
-                     and tempG.status = 0
-                   group by status) as LoserShoots,
-                 (select count(*)
-                    from games as tempG
-                   where tempG.player_id = G.player_id
-                     and tempG.status = 1
-                   group by status) as WinnerShoots,
-                 ((select count(*)
-                    from games as tempG
-                   where tempG.player_id = G.player_id
-                     and tempG.status = 1
-                   group by status) * 100) / count(*) as Promedio
-            FROM games as G join players as P on (G.player_id = P.id)
-           GROUP BY player_id, P.name
-           ORDER BY Promedio DESC";
+        $sql = "SELECT P.id, P.name,
+                      (select count(*)
+                        from games as tempG1
+                        where tempG1.player_id = P.id
+                        group by tempG1.player_id) as Total,
+                      (select count(*)
+                        from games as tempG2
+                        where tempG2.player_id = P.id
+                          and tempG2.status = 0
+                        group by status) as LoserShoots,
+                      (select count(*)
+                        from games as tempG3
+                        where tempG3.player_id = P.id
+                          and tempG3.status = 1
+                        group by status) as WinnerShoots,
+                      (
+                      (select count(*)
+                        from games as tempG4
+                        where tempG4.player_id = P.id
+                          and tempG4.status = 1
+                        group by status) * 100)
+                      /
+                      (select count(*)
+                        from games as tempG5
+                        where tempG5.player_id = P.id
+                        group by tempG5.player_id)
+                      as Promedio
+                FROM games as G right join players as P on (G.player_id = P.id)
+                GROUP BY P.id, P.name
+                ORDER BY Promedio DESC";
         $ranking = DB::select($sql);
         return response()->json(['ranking' => $ranking], 200);
     }
 
     /* Retorna el jugador amb pitjor percentatge d’èxit */
     public function rankingLoser () {
-        $sql = "SELECT player_id, P.name, count(*) as Total,
-                 (select count(*)
-                    from games as tempG
-                   where tempG.player_id = G.player_id
-                     and tempG.status = 0
-                   group by status) as LoserShoots,
-                 (select count(*)
-                    from games as tempG
-                   where tempG.player_id = G.player_id
-                     and tempG.status = 1
-                   group by status) as WinnerShoots,
-                 ((select count(*)
-                    from games as tempG
-                   where tempG.player_id = G.player_id
-                     and tempG.status = 1
-                   group by status) * 100) / count(*) as Promedio
-            FROM games as G join players as P on (G.player_id = P.id)
-           GROUP BY player_id, P.name
-           ORDER BY Promedio ASC";
+        $sql = "SELECT P.id, P.name,
+                      (select count(*)
+                        from games as tempG1
+                        where tempG1.player_id = P.id
+                        group by tempG1.player_id) as Total,
+                      (select count(*)
+                        from games as tempG2
+                        where tempG2.player_id = P.id
+                          and tempG2.status = 0
+                        group by status) as LoserShoots,
+                      (select count(*)
+                        from games as tempG3
+                        where tempG3.player_id = P.id
+                          and tempG3.status = 1
+                        group by status) as WinnerShoots,
+                      (
+                      (select count(*)
+                        from games as tempG4
+                        where tempG4.player_id = P.id
+                          and tempG4.status = 1
+                        group by status) * 100)
+                      /
+                      (select count(*)
+                        from games as tempG5
+                        where tempG5.player_id = P.id
+                        group by tempG5.player_id)
+                      as Promedio
+                FROM games as G right join players as P on (G.player_id = P.id)
+                GROUP BY P.id, P.name
+                ORDER BY Promedio ASC";
         $player = DB::select($sql)[0];
         return response()->json(['player' => $player], 200);
     }
 
     /* Retorna el jugador amb pitjor percentatge d’èxit. */
     public function rankingWinner () {
-        $sql = "SELECT player_id, P.name, count(*) as Total,
-                 (select count(*)
-                    from games as tempG
-                   where tempG.player_id = G.player_id
-                     and tempG.status = 0
-                   group by status) as LoserShoots,
-                 (select count(*)
-                    from games as tempG
-                   where tempG.player_id = G.player_id
-                     and tempG.status = 1
-                   group by status) as WinnerShoots,
-                 ((select count(*)
-                    from games as tempG
-                   where tempG.player_id = G.player_id
-                     and tempG.status = 1
-                   group by status) * 100) / count(*) as Promedio
-            FROM games as G join players as P on (G.player_id = P.id)
-           GROUP BY player_id, P.name
-           ORDER BY Promedio DESC";
+        $sql = "SELECT P.id, P.name,
+                        (select count(*)
+                          from games as tempG1
+                          where tempG1.player_id = P.id
+                          group by tempG1.player_id) as Total,
+                        (select count(*)
+                          from games as tempG2
+                          where tempG2.player_id = P.id
+                            and tempG2.status = 0
+                          group by status) as LoserShoots,
+                        (select count(*)
+                          from games as tempG3
+                          where tempG3.player_id = P.id
+                            and tempG3.status = 1
+                          group by status) as WinnerShoots,
+                        (
+                        (select count(*)
+                          from games as tempG4
+                          where tempG4.player_id = P.id
+                            and tempG4.status = 1
+                          group by status) * 100)
+                        /
+                        (select count(*)
+                          from games as tempG5
+                          where tempG5.player_id = P.id
+                          group by tempG5.player_id)
+                        as Promedio
+                  FROM games as G right join players as P on (G.player_id = P.id)
+                  GROUP BY P.id, P.name
+                  ORDER BY Promedio DESC";
         $player = DB::select($sql)[0];
         return response()->json(['player' => $player], 200);
     }
